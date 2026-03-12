a8a6929c5d6c90d196a2e2bcdd4778ccd082e59f
Jonah Elliott <jonahelliott@u.boisestate.edu>
2026-03-12 15:32:41 -0600

    Removed commented code for four_mux

--- 4_mux.v
+++ 4_mux.v
@@ -4,16 +4,6 @@ module four_mux (
     input [1:0] Sel,
     output [3:0] Y
 );
-//    assign Y = (CEO & ~Sel[0] & ~Sel[1] |
-//                You &  Sel[0] & ~Sel[1] |
-//                Fred & ~Sel[0] &  Sel[1] |
-//                Jill &  Sel[0] &  Sel[1] |) & Enable;
-                
-//    equivalence_check equivalence_inst (
-//        .select[0](Sel[0]),
-//        .select[1](Sel[1]),
-//        .
-//    );
     
     assign Y = Sel == 2'b00 && Enable == 1? CEO :
                 Sel == 2'b01 && Enable == 1? You :
@@ -23,20 +13,3 @@ module four_mux (
         
 
 endmodule
-
-//Old solution
-//module 4_mux(
-//    input CEO, You, Fred, Jill, Enable,
-//    input Sel[1:0],
-//    output Y
-//);
-
-//    assign Y = (A & ~Sel[0] & ~Sel[1] |
-//                B &  Sel[0] & ~Sel[1] |
-//                C & ~Sel[0] &  Sel[1] |
-//                D &  Sel[0] &  Sel[1] |) & Enable;
-
-//endmodule
-
-//endmodule
-
